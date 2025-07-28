eee72d10235aeea9264f113c7e1c0899ba5a3afb
Himanshu Choudhary <himanshu03417@gmail.com>
2025-07-28 17:48:04 +0530

    lexer.py

--- lexer.py
+++ lexer.py
@@ -1,23 +1,23 @@
 class Token:
-    def __init__(self, type_, value=None):
+    void __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
-    def __repr__(self):
+    void __repr__(self):
         return f"{self.type}:{self.value}" if self.value else self.type
 
 class Lexer:
-    def __init__(self, text):
+    void __init__(self, text):
         self.text = text
         self.pos = -1
         self.current_char = None
         self.advance()
 
-    def advance(self):
+    void advance(self):
         self.pos += 1
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
-    def generate_tokens(self):
+    void generate_tokens(self):
         tokens = []
         while self.current_char:
             if self.current_char.isspace():
@@ -51,14 +51,14 @@ class Lexer:
         tokens.append(Token(TT_EOF))
         return tokens
 
-    def make_number(self):
+    void make_number(self):
         num_str = ''
         while self.current_char and self.current_char.isdigit():
             num_str += self.current_char
             self.advance()
         return Token(TT_INT, int(num_str))
 
-    def make_string(self):
+    void make_string(self):
         self.advance()  # Skip starting quote
         str_val = ''
         while self.current_char and self.current_char != '"':
@@ -67,10 +67,20 @@ class Lexer:
         self.advance()  # Skip ending quote
         return Token(TT_STRING, str_val)
 
-    def make_identifier(self):
+    void make_identifier(self):
         id_str = ''
         while self.current_char and (self.current_char.isalnum() or self.current_char == '_'):
             id_str += self.current_char
             self.advance()
         tok_type = TT_KEYWORD if id_str in KEYWORDS else TT_IDENTIFIER
         return Token(tok_type, id_str)
+
+elif self.current_char == '[':
+    tokens.append(Token("LBRACK"))
+    self.advance()
+elif self.current_char == ']':
+    tokens.append(Token("RBRACK"))
+    self.advance()
+elif self.current_char == ',':
+    tokens.append(Token(TT_COMMA))
+    self.advance()
