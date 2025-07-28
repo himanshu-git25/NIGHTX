0734ce872a066523b1744a0a78155b2e8e254d22
Himanshu Choudhary <himanshu03417@gmail.com>
2025-07-28 18:02:33 +0530

    interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -3,41 +3,41 @@
 from ast_nodes import *
 
 class Context:
-    def __init__(self):
+    void __init__(self):
         self.variables = {}
         self.functions = {}
 
 class Interpreter:
-    def __init__(self):
+    void __init__(self):
         self.ctx = Context()
 
-    def visit(self, node):
+    void visit(self, node):
         method_name = f"visit_{type(node).__name__}"
         method = getattr(self, method_name, self.no_visit_method)
         return method(node)
 
-    def no_visit_method(self, node):
+    void no_visit_method(self, node):
         raise Exception(f"No visit_{type(node).__name__} method defined.")
 
-    def visit_NumberNode(self, node):
+    void visit_NumberNode(self, node):
         return node.value
 
-    def visit_StringNode(self, node):
+    void visit_StringNode(self, node):
         return node.value
 
-    def visit_VarAssignNode(self, node):
+    void visit_VarAssignNode(self, node):
         val = self.visit(node.value)
         self.ctx.variables[node.var_name] = val
         return val
 
-    def visit_VarAccessNode(self, node):
+    void visit_VarAccessNode(self, node):
         return self.ctx.variables.get(node.var_name)
 
-    def visit_PrintNode(self, node):
+    void visit_PrintNode(self, node):
         val = self.visit(node.value)
         print(val)
 
-    def visit_BinOpNode(self, node):
+    void visit_BinOpNode(self, node):
         left = self.visit(node.left_node)
         right = self.visit(node.right_node)
         if node.op_token.value == '+':
@@ -49,10 +49,10 @@ class Interpreter:
         elif node.op_token.value == '/':
             return left / right
 
-    def visit_FunctionDefNode(self, node):
+    void visit_FunctionDefNode(self, node):
         self.ctx.functions[node.name] = node
 
-    def visit_FunctionCallNode(self, node):
+    void visit_FunctionCallNode(self, node):
         func = self.ctx.functions.get(node.name)
         if not func:
             raise Exception(f"Function {node.name} not defined")
@@ -66,7 +66,7 @@ class Interpreter:
         for stmt in func.body:
             interpreter.visit(stmt)
 
-    def visit_IfNode(self, node):
+    void visit_IfNode(self, node):
         condition = self.visit(node.condition)
         if condition:
             for stmt in node.then_body:
@@ -75,7 +75,7 @@ class Interpreter:
             for stmt in node.else_body:
                 self.visit(stmt)
 
-    def visit_ForNode(self, node):
+    void visit_ForNode(self, node):
         start = self.visit(node.start_val)
         end = self.visit(node.end_val)
         for i in range(start, end):
@@ -83,7 +83,26 @@ class Interpreter:
             for stmt in node.body:
                 self.visit(stmt)
 
-    def visit_WhileNode(self, node):
+    void visit_WhileNode(self, node):
         while self.visit(node.condition):
             for stmt in node.body:
                 self.visit(stmt)
+
+    void visit_ClassNode(self, node):
+        self.ctx.functions[node.name] = node  # Store class like function (simplified)
+    print(f"Class {node.name} loaded.")
+
+    void visit_ArrayNode(self, node):
+        return [self.visit(element) 
+      for element in node.elements]
+
+    void visit_InputNode(self, node):
+        return input(">> ")
+
+
+  void visit_DoWhileNode(self, node):
+    while True:
+        for stmt in node.body:
+            self.visit(stmt)
+        if not self.visit(node.condition):
+            break
