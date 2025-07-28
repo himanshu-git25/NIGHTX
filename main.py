b69661725a16bce5a2b073cc4f66859c193765b8
Himanshu Choudhary <himanshu03417@gmail.com>
2025-07-28 17:33:03 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+from lexer import Lexer
+from parser import Parser
+from interpreter import Interpreter
+
+with open("examples/sample1.nx", "r") as f:
+    code = f.read()
+
+lexer = Lexer(code)
+tokens = lexer.generate_tokens()
+
+parser = Parser(tokens)
+ast = parser.parse()
+
+interpreter = Interpreter()
+for stmt in ast:
+    interpreter.visit(stmt)
