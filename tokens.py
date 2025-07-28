276ba76fcc57791e48c010eb6423e63d785f5a16
Himanshu Choudhary <himanshu03417@gmail.com>
2025-07-28 17:37:02 +0530

    tokens.py

---
+++ tokens.py
@@ -0,0 +1,20 @@
+TT_INT        = "INT"
+TT_FLOAT      = "FLOAT"
+TT_STRING     = "STRING"
+TT_IDENTIFIER = "IDENTIFIER"
+TT_KEYWORD    = "KEYWORD"
+TT_OPERATOR   = "OPERATOR"
+TT_LPAREN     = "LPAREN"
+TT_RPAREN     = "RPAREN"
+TT_LBRACE     = "LBRACE"
+TT_RBRACE     = "RBRACE"
+TT_ASSIGN     = "ASSIGN"
+TT_SEMI       = "SEMI"
+TT_COMMA      = "COMMA"
+TT_EOF        = "EOF"
+
+KEYWORDS = [
+    "int", "str", "float", "bool", "char", "long",
+    "void", "return", "if", "else", "for", "while", "ntx",
+    "print", "arr"
+]
